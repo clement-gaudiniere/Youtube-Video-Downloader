7eb2f484dbfaf1c4ad47ad5ead590b8ecedc9b8b
clement-gaudiniere <clementlyara72@gmail.com>
2022-01-26 15:33:55 +0100

    V1.0.3

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-document.getElementById("myButton").addEventListener("click", operatingSytem);
+document.getElementById("getUrl").addEventListener("click", getUrl);
 
 
-function operatingSytem() {
-  var OSName="Unknown OS";
-  if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-  if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
-  if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
-  if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
-
-  // Display the OS name
-  document.getElementById("OS").innerHTML = OSName;
+function getUrl() {
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    console.log(tabs[0].url);
+    document.getElementById("url").innerHTML = tabs[0].url;
+  });
 }
-
-
-// Si l'utilisateur active le theme
-let checkbox = document.getElementById("active");
-
-checkbox.addEventListener('change', function() {
-  if(this.checked) {
-    alert('checked');
-  } else {
-    alert('unchecked');
-  }
-});
